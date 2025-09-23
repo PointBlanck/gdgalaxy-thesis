d003bcb9c059b8b53bbd31f408761cd08f2d9c69
PointBlanck <kavalagiost@gmail.com>
2025-09-24 00:03:14 +0300

    Add capability to interactivily input parameters for potential plotting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """ Main module of thesis. """
 
+# Import necessary modules
 import precalculation as pc
 
 def main():
@@ -10,10 +11,19 @@ def main():
     print("Numeric calculation of spiral-maintaining family of orbits in Grand Design galaxies")
     print(100*"=")
 
-    # Ask use for input
+    # Ask user for input
     precalc = input("Q1: Would you like to inspect the model via plots?")
     if precalc.lower() == "y":
-        pc.plot_potentials()
+        max_r = input("Input: max_r\n")
+        max_r = int(max_r)
+        text = input("Input: dim(r), dim(phi)\n")
+        N_r, N_phi = text.split(",")
+        N_r = int(N_r.strip())
+        N_phi = int(N_phi.strip())
+        print("Plotting potentials...")
+        pc.plot_potentials(max_r, N_r, N_phi)
+    elif precalc.lower() == "n":
+        print("\nPrecalculation denied.")
     else:
         return 1
     print("Main terminated successfully")
